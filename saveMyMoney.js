bbcb10f1e0fe29b3c9a5cf44316c3e3c6d8a2359
Fahad Ali <fahad.ali@progstream.com>
2019-12-12 19:10:33 +0500

    initial commit

---
+++ saveMyMoney.js
@@ -0,0 +1,44 @@
+
+let numberOfBars = 17;
+const pricesOfBarPacketBox = [2.30, 25, 230];
+const quantityOfBarPacketBox = [1, 12, 12 * 10];
+
+function calculateBars(bars, barPacket) {
+    return bars % barPacket;
+}
+
+function calculateNumberOfPacks(bars, packet) {
+    return Math.floor(bars / packet);
+}
+
+function calculateNumberOfBoxes(numOfPack,boxes) {
+    let box = Math.floor(boxes/12);
+    return Math.floor(numOfPack / box);
+} 
+
+function calculateTotalCost(numOfBars, numOfPack, numOfBoxes, prices) {
+    let packet = numOfPack;
+    if(numOfBoxes > 0) {
+        packet = numOfPack - 10;
+    }
+    let cost = (numOfBars * prices[0]) + (packet * prices[1]) + (numOfBoxes * prices[2]);
+    return cost;
+}
+
+function saveMyMoney(numberOfBars, pricesOfBarPacketBox, quantityOfBarPacketBox) {
+
+    let numOfBars = calculateBars(numberOfBars, quantityOfBarPacketBox[1]);
+    let numOfPack = calculateNumberOfPacks(numberOfBars, quantityOfBarPacketBox[1]);
+    let numOfBoxes = calculateNumberOfBoxes(numOfPack,quantityOfBarPacketBox[2]);
+    let cost = calculateTotalCost(numOfBars, numOfPack, numOfBoxes, pricesOfBarPacketBox);
+    
+    let payload = {
+        "numberOfBars" : numOfBars,
+        "numberOfPacks" : numOfPack,
+        "numberOfBoxes" : numOfBoxes,
+        "totalCost" : cost
+    }
+    console.log('cost', payload)
+}
+
+saveMyMoney(numberOfBars, pricesOfBarPacketBox, quantityOfBarPacketBox)
